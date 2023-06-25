80e28490f524abdf49a861f46064f3e36df29fbb
Victor Oliveira <victoroliveira@MacBook-Air-de-Victor.local>
2023-06-25 13:44:53 -0300

    Remove outdated Flyway scripts and consolidate changes in v1 migration

--- persistence/src/main/resources/db/migration/V1__Initial_tables.sql
+++ persistence/src/main/resources/db/migration/V1__Initial_tables.sql
@@ -4,5 +4,24 @@ CREATE TABLE product(
     `name` VARCHAR(255) NOT NULL,
     `description` VARCHAR(255) NOT NULL,
     `value` DOUBLE NOT NULL,
+    `created_at` DATETIME DEFAULT NOW(),
+    `modified_at` DATETIME DEFAULT NOW(),
+    `deleted_at` DATETIME DEFAULT NULL,
     CONSTRAINT `pk_id` PRIMARY KEY (id)
 );
+
+CREATE INDEX ix_deleted_at ON product (deleted_at);
+CREATE INDEX ix_category ON product (category);
+
+CREATE TABLE client(
+    `id` BIGINT NOT NULL AUTO_INCREMENT,
+    `cpf` VARCHAR(11) NOT NULL,
+    `email` VARCHAR(255) NOT NULL,
+    `name` VARCHAR(255) NOT NULL,
+    `created_at` DATETIME DEFAULT NOW(),
+    `modified_at` DATETIME DEFAULT NOW(),
+    `deleted_at` DATETIME DEFAULT NULL,
+    CONSTRAINT `pk_id` PRIMARY KEY (id)
+);
+
+CREATE INDEX ix_deleted_at ON client (deleted_at);
