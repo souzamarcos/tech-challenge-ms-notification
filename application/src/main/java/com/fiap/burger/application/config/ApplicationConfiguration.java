b46cc464b7eba1f8efa6eed116990ea81f24ecd5
Marcos Souza <markin.mv.amaral@gmail.com>
2023-08-29 12:24:41 -0300

    chore: Remove unnecessary annotation

--- application/src/main/java/com/fiap/burger/application/config/ApplicationConfiguration.java
+++ application/src/main/java/com/fiap/burger/application/config/ApplicationConfiguration.java
@@ -14,7 +14,6 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("com.fiap.burger")
 public class ApplicationConfiguration {
 
     @Bean
