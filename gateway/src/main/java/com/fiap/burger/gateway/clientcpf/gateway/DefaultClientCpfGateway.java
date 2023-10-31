3a25e8972463721896ccd2796bf97865455e64db
Marcos Souza <markin.mv.amaral@gmail.com>
2023-10-31 20:03:24 -0300

    feature: Add localstack

--- gateway/src/main/java/com/fiap/burger/gateway/clientcpf/gateway/DefaultClientCpfGateway.java
+++ gateway/src/main/java/com/fiap/burger/gateway/clientcpf/gateway/DefaultClientCpfGateway.java
@@ -2,6 +2,7 @@ package com.fiap.burger.gateway.clientcpf.gateway;
 
 import com.fiap.burger.gateway.clientcpf.model.ClientCpfModel;
 import com.fiap.burger.usecase.adapter.gateway.ClientCpfGateway;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 import software.amazon.awssdk.auth.credentials.ProfileCredentialsProvider;
@@ -15,29 +16,22 @@ import software.amazon.awssdk.services.dynamodb.DynamoDbClient;
 @Repository
 public class DefaultClientCpfGateway implements ClientCpfGateway {
 
-    @Value("${dynamodb.tablename}")
-    private String tableName;
-    private final DynamoDbClient dynamoDbClient =
-        DynamoDbClient.builder()
-            .region(Region.US_EAST_1)
-            .credentialsProvider(ProfileCredentialsProvider.create())
-            .build();
-    private final DynamoDbEnhancedClient enhancedClient =
-        DynamoDbEnhancedClient.builder()
-            .dynamoDbClient(dynamoDbClient)
-            .build();
+    private DynamoDbEnhancedClient enhancedClient;
+    private DynamoDbTable<ClientCpfModel> table;
 
+    DefaultClientCpfGateway(DynamoDbEnhancedClient enhancedClient, @Value("${dynamodb.tablename}") String tableName) {
+        this.enhancedClient = enhancedClient;
+        this.table = enhancedClient.table(tableName, TableSchema.fromBean(ClientCpfModel.class));
+    }
     @Override
     public void save(String cpf, Long clientId) {
-        getTable().putItem(new ClientCpfModel(cpf, clientId));
+        table.putItem(new ClientCpfModel(cpf, clientId));
     }
 
     @Override
     public Long findByCpf(String cpf) {
-        return getTable().getItem(Key.builder().partitionValue(cpf).build()).getId();
+        return table.getItem(Key.builder().partitionValue(cpf).build()).getId();
     }
 
-    private DynamoDbTable<ClientCpfModel> getTable() {
-        return enhancedClient.table(tableName, TableSchema.fromBean(ClientCpfModel.class));
-    }
+
 }
