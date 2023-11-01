5fc76362d319f65168c5f29ddecf1c3bdd08ad42
Guilherme Henrique <ghvoliveira@gmail.com>
2023-11-01 20:18:12 -0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -85,6 +85,6 @@ tasks.test {
 
 sonarqube {
 	properties {
-		property("sonar.exclusions", "**/entity/**,**/secret/**",**/*Configuration.java"))
+		property("sonar.exclusions", "**/entity/**,**/secret/**,**/*Configuration.java"))
 	}
 }
