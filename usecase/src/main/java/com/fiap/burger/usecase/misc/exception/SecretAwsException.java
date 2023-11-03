d64dd160ffe35edff089bdba3689a2e200b7869f
Marcos Souza <markin.mv.amaral@gmail.com>
2023-11-03 12:07:50 -0300

    chore: Remove EnvironmentVariableCredentialsProvider

--- usecase/src/main/java/com/fiap/burger/usecase/misc/exception/SecretAwsException.java
+++ usecase/src/main/java/com/fiap/burger/usecase/misc/exception/SecretAwsException.java
@@ -5,6 +5,9 @@ public class SecretAwsException extends DomainException {
     public SecretAwsException(String message) {
         super(message);
     }
+    public SecretAwsException(String message, Throwable cause) {
+        super(message, cause);
+    }
 
 }
 
