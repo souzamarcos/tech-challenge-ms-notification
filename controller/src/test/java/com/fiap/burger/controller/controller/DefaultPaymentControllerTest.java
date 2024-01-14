411f20d8a798dc995a5b0af226a90049fb2936fa
Guilherme Oliveira <ghvoliveira@gmail.com>
2024-01-13 21:36:59 -0300

    Removendo classes desnecessárias e melhorando testes

--- controller/src/test/java/com/fiap/burger/controller/controller/DefaultPaymentControllerTest.java
+++ controller/src/test/java/com/fiap/burger/controller/controller/DefaultPaymentControllerTest.java
@@ -6,6 +6,7 @@ import com.fiap.burger.messenger.payment.DefaultPaymentMessenger;
 import com.fiap.burger.usecase.misc.exception.PaymentNotFoundException;
 import com.fiap.burger.usecase.usecase.DefaultPaymentUseCase;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -35,93 +36,109 @@ class DefaultPaymentControllerTest {
         MockitoAnnotations.openMocks(this);
     }
 
-    @Test
-    void shouldFindById() {
-        var id = 1L;
-        var expected = new Payment(1L, PaymentStatus.ABERTO);
+    @Nested
+    class findById {
+        @Test
+        void shouldFindById() {
+            var id = 1L;
+            var expected = new Payment(1L, PaymentStatus.ABERTO);
 
-        when(useCase.findById(id)).thenReturn(expected);
+            when(useCase.findById(id)).thenReturn(expected);
 
-        Payment actual = controller.findById(id);
+            Payment actual = controller.findById(id);
 
-        assertEquals(expected, actual);
+            assertEquals(expected, actual);
 
-        verify(useCase, times(1)).findById(id);
+            verify(useCase, times(1)).findById(id);
+        }
+
+        @Test
+        void shouldThrownPaymentNotFoundExceptionWhenPaymentNotFoundById() {
+            var id = 1L;
+
+            when(useCase.findById(id)).thenReturn(null);
+
+            assertThrows(PaymentNotFoundException.class, () -> controller.findById(id));
+
+            verify(useCase, times(1)).findById(id);
+        }
     }
 
-    @Test
-    void shouldThrownPaymentNotFoundExceptionWhenPaymentNotFoundById() {
-        var id = 1L;
+    @Nested
+    class findByOrderId {
+        @Test
+        void shouldFindByOrderId() {
+            var orderId = 1L;
+            var expected = List.of(new Payment(1L, PaymentStatus.ABERTO));
 
-        when(useCase.findById(id)).thenReturn(null);
+            when(useCase.findByOrderId(orderId)).thenReturn(expected);
 
-        assertThrows(PaymentNotFoundException.class, () -> controller.findById(id));
+            List<Payment> actual = controller.findByOrderId(orderId);
 
-        verify(useCase, times(1)).findById(id);
+            assertEquals(expected, actual);
+
+            verify(useCase, times(1)).findByOrderId(orderId);
+        }
+
+        @Test
+        void shouldThrownPaymentNotFoundExceptionWhenPaymentNotFoundByOrderId() {
+            var orderId = 1L;
+
+            when(useCase.findByOrderId(orderId)).thenReturn(null);
+
+            assertThrows(PaymentNotFoundException.class, () -> controller.findByOrderId(orderId));
+
+            verify(useCase, times(1)).findByOrderId(orderId);
+        }
     }
 
-    @Test
-    void shouldFindByOrderId() {
-        var orderId = 1L;
-        var expected = List.of(new Payment(1L, PaymentStatus.ABERTO));
+    @Nested
+    class insertPayment {
+        @Test
+        void shouldInsertPayment() {
+            var orderId = 1L;
+            var payment = new Payment(1L, PaymentStatus.ABERTO);
 
-        when(useCase.findByOrderId(orderId)).thenReturn(expected);
+            when(useCase.insert(orderId)).thenReturn(payment);
 
-        List<Payment> actual = controller.findByOrderId(orderId);
+            Payment actual = controller.insert(orderId);
 
-        assertEquals(expected, actual);
+            assertEquals(payment, actual);
 
-        verify(useCase, times(1)).findByOrderId(orderId);
+            verify(useCase, times(1)).insert(orderId);
+            verify(messenger, times(1)).sendMessage(actual);
+        }
     }
 
-    @Test
-    void shouldThrownPaymentNotFoundExceptionWhenPaymentNotFoundByOrderId() {
-        var orderId = 1L;
+    @Nested
+    class updateStatusPayment {
+        @Test
+        void shouldUpdateStatusPaymentToApproved() {
+            var id = 1L;
+            var orderId = 1L;
+            var persistedPayment = new Payment(1L, orderId, PaymentStatus.APROVADO);
 
-        when(useCase.findByOrderId(orderId)).thenReturn(null);
+            when(useCase.updateStatus(id, PaymentStatus.APROVADO)).thenReturn(persistedPayment);
 
-        assertThrows(PaymentNotFoundException.class, () -> controller.findByOrderId(orderId));
+            controller.updateStatus(id, PaymentStatus.APROVADO);
 
-        verify(useCase, times(1)).findByOrderId(orderId);
+            verify(useCase, times(1)).updateStatus(id, PaymentStatus.APROVADO);
+            verify(messenger, times(1)).sendMessage(persistedPayment);
+        }
+
+        @Test
+        void shouldUpdateStatusPaymentToRefused() {
+            var id = 1L;
+            var orderId = 1L;
+            var persistedPayment = new Payment(1L, orderId, PaymentStatus.RECUSADO);
+
+            when(useCase.updateStatus(id, PaymentStatus.RECUSADO)).thenReturn(persistedPayment);
+
+            controller.updateStatus(id, PaymentStatus.RECUSADO);
+
+            verify(useCase, times(1)).updateStatus(id, PaymentStatus.RECUSADO);
+            verify(messenger, times(1)).sendMessage(persistedPayment);
+        }
     }
 
-    @Test
-    void shouldInsertPayment() {
-        var orderId = 1L;
-        var payment = new Payment(1L, PaymentStatus.ABERTO);
-
-        when(useCase.insert(orderId)).thenReturn(payment);
-
-        Payment actual = controller.insert(orderId);
-
-        assertEquals(payment, actual);
-
-        verify(useCase, times(1)).insert(orderId);
-    }
-
-    @Test
-    void shouldUpdateStatusPaymentToApproved() {
-        var id = 1L;
-        var orderId = 1L;
-        var persistedPayment = new Payment(1L, orderId, PaymentStatus.APROVADO);
-
-        when(useCase.updateStatus(id, PaymentStatus.APROVADO)).thenReturn(persistedPayment);
-
-        controller.updateStatus(id, PaymentStatus.APROVADO);
-
-        verify(useCase, times(1)).updateStatus(id, PaymentStatus.APROVADO);
-    }
-
-    @Test
-    void shouldUpdateStatusPaymentToRefused() {
-        var id = 1L;
-        var orderId = 1L;
-        var persistedPayment = new Payment(1L, orderId, PaymentStatus.RECUSADO);
-
-        when(useCase.updateStatus(id, PaymentStatus.RECUSADO)).thenReturn(persistedPayment);
-
-        controller.updateStatus(id, PaymentStatus.RECUSADO);
-
-        verify(useCase, times(1)).updateStatus(id, PaymentStatus.RECUSADO);
-    }
 }
